a4b643878667004d9bc1bc2d3856d95c847be922
Stas <staskolodyuk@gmail.com>
2016-10-22 20:59:23 +0300

    Initial Economics Risks Counter

--- js/plot.js
+++ js/plot.js
@@ -1,166 +1,89 @@
-var currentIndexSelected;
-var data;
-data = [
-  [1, 29.9],
-  [2, 71.5],
-  [3, 106.4],
-  [4, 129.2],
-  [5, 144.0],
-  [6, 176.0],
-  [7, 135.6],
-  [8, 148.5],
-  [9, 216.4],
-  [10, 194.1],
-  [11, 95.6],
-  [12, 54.4]
-];
-var chart = new Highcharts.Chart({
-  chart: {
-    renderTo: 'container',
-    defaultSeriesType: 'line',
-    events: {
-      click: function(e) {
-        // find the clicked values and the series
-        var x = e.xAxis[0].value,
-          y = e.yAxis[0].value,
-          series = this.series[0],
-          xIndex = getIndex(x, series.xData), //get 'index'
-          data = series.options.data; //get original data
+var income = [[14, 0], [15, 0.7], [16, 1], [17, 0.9], [18, 0.5], [19, 0.1], [20, 0]];
+var credit = [[10, 0], [11, 0.2], [12, 0.8], [13, 1], [14, 0.9], [15, 0.4], [16, 0]];
+var profit = [[-2, 0], [-1, 0], [0, 0.4], [1, 0.7], [2, 0.9], [3, 1], [4, 0.9], [5, 0.8], [6, 0.5], [7, 0.2], [8, 0.1], [9, 0], [10, 0]];
+var insolvency = [[-3, 1], [-2, 0.8], [-1, 0.7], [0, 0.6], [1, 0.3], [2, 0.1], [3, 0]];
 
+var incomeChart = createChart('income', income, redrawFunction);
+var incomeSeries = incomeChart.series[0];
 
-        if (xIndex > 0) { //add new point only when X-value is different
-          data.splice(xIndex, 0, [x, y]);
-          series.setData(data);
-        }
+var creditChart = createChart('credit', credit, redrawFunction);
+var creditSeries = creditChart.series[0];
 
+var insolvencyChart = createChart('insolvency', insolvency, redrawFunction);
+var insolvencySeries = insolvencyChart.series[0];
 
-        function getIndex(x, data) {
-          var index = 0,
-            dLen = data.length;
-          for (var i = 0; i < dLen; i++) {
-            if (data[i] > x) {
-              index = i;
-              i = dLen;
-            }
-          }
-          if (x == data[index]) {
-            return -1;
-          } else {
-            return index;
-          }
+var profitChart = createChart('profit', profit);
+var profitSeries = profitChart.series[0];
 
-        }
-      }
+function redrawFunction(series) {
+  var profit = {};
+
+  for(var i = 0; i < incomeSeries.xData.length; i++) {
+    for(var j = 0; j < creditSeries.yData.length; j++) {
+      profit[incomeSeries.xData[i] - creditSeries.xData[j]] = Math.max(Math.min(incomeSeries.yData[i], creditSeries.yData[j]), profit[incomeSeries.xData[i] - creditSeries.xData[j]] || 0);
     }
-  },
-  xAxis: {
-
-  },
-  yAxis: {},
-  legend: {
-    layout: 'vertical',
-    floating: true,
-    backgroundColor: '#FFFFFF',
-    align: 'right',
-    verticalAlign: 'top',
-    y: 60,
-    x: -60
-  },
-  tooltip: {
-    formatter: function() {
-      return '<b>' + this.series.name + '</b><br/>' + this.x + ': ' + this.y;
-    }
-  },
-  plotOptions: {},
-  series: [{
-    allowPointSelect: true,
-    cursor: 'ns-resize',
-    point: {
-      events: {
-        select: function() {
-          var pId = this.series.data.indexOf(this);
-          currentIndexSelected = pId;
-
-
-          $('#out').html(
-            'Dragging <b>' + this.series.points[pId].y); //this.series.data[0].x + '</b>, <b>');
-
-        },
-
-        drag: function(e) {
-
-          // Returning false stops the drag and drops. Example:
-          /*
-              if (e.newY > 300) {
-                  this.y = 300;
-                  return false;
-              }
-              */
-          $('#drag').html(
-            'Dragging <b>' + this.series.name + '</b>, <b>' + this.category + '</b> to <b>' + Highcharts.numberFormat(e.newY, 2) + '</b>');
-        },
-        drop: function() {
-          $('#drop').html(
-            'In <b>' + this.series.name + '</b>, <b>' + this.category + '</b> was set to <b>' + Highcharts.numberFormat(this.y, 2) + '</b>');
-        }
-      }
-    },
-    data: data,
-    //data: [[1,29.9], [2,71.5],[3,106.4], [4,129.2], [5,144.0], [6,176.0], [7,135.6], [8,148.5], [9,216.4], [10,194.1], [11,95.6], [12,54.4]],
-    draggableX: true,
-    draggableY: true,
-
-  }]
-});
-
-
-
-
-// button handler
-$('#button1').click(function() {
-  //this.point[currentIndexSelected].remove();
-  var chart = $('#container').highcharts();
-  if (chart.series.length == 1) {
-    //chart.series[0].remove();
-    chart.addSeries({
-      data: [194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4],
-      draggableX: true,
-      draggableY: true
-    });
   }
 
+  profit = Object.keys(profit)
+                 .map(key => parseFloat(key))
+                 .sort((a,b) => a - b)
+                 .map(key => [key, profit[key]]);
 
+  var risk = 0;
 
+  for(var i = 0; i < profitSeries.xData.length; i++) {
+    for(var j = 0; j < insolvencySeries.xData.length; j++) {
+      if(profitSeries.xData[i] == insolvencySeries.xData[j]) {
+        risk = Math.max(Math.min(profitSeries.yData[i], insolvencySeries.yData[j]), risk);
+      } else if (profitSeries.xData[i] < insolvencySeries.xData[0]) {
+        risk = Math.max(insolvencySeries.yData[0], risk);
+      }
+    }
+  }
 
-  var series = chart.getSelectedSeries();
-  $('#out').html('length:' + series.length);
-  // if(data.length<series[0].data.length){
-  //    $('#out').html('smaller');
-  // }
-  /* for(var i=0; i<series.data.length; i++){
-       data[i] = series.data[i];
-  }*/
-  //if (series.data.length) {
-  // categories.splice(currentIndexSelected,1);
-  // data.splice(currentIndexSelected, 1);
+  $('#risk').text(risk.toFixed(2));
 
-  //var newSerie =
-  //chart.series[1].setData(data);
-  //chart.redraw();
-  //chart.xAxis[0].setCategories(categories);
-  //}
-});
+  profitSeries.setData(profit, true);
+}
 
-// button handler
-$('#button2').click(function() {
-  //this.point[currentIndexSelected].remove();
-  //var series = chart.series[0];
-  var series = chart.getSelectedSeries();
-
-  //categories.splice(currentIndexSelected,1);
-  data.splice(currentIndexSelected, 1);
-
-  chart.series[0].setData(data);
-  chart.xAxis[0].setCategories(categories);
-
-});
+function createChart(seriesName, data, onDataChangeFunction) {
+    return new Highcharts.Chart({
+        title: {
+           text: seriesName
+        },
+        chart: {
+            renderTo: seriesName,
+            animation: false,
+            /*events: {
+              click: function(e) {
+                if(onDataChangeFunction) {
+                  this.series[0].addPoint([e.xAxis[0].value, e.yAxis[0].value]);
+                  onDataChangeFunction();
+                }
+              }
+            }*/
+        },
+        series: [{
+            name: seriesName,
+            type: 'line',
+            cursor: 'move',
+            //draggableX: true,
+            draggableY: true,
+            data: data
+        }],
+        plotOptions: {
+            series: {
+                point: {
+                    events: {
+                        drop: function() {
+                          redrawFunction(this.series.data);
+                        },
+                        click: function() {
+                           redrawFunction(this.series.data);
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
